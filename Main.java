08529d9930d1058b2c4bd055ced67f63e9f40ac2
Eloisa F. Perdiguerra <161401160+Loi-sa@users.noreply.github.com>
2024-12-07 15:09:52 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,5 +2,16 @@ public class Main{
     public static void main(String[] args) {
     System.out.println("Hello Java");
     }
+
+    class Tasksheet112 {
+    byte zero = 0;
+    String output ="W"+zero+"w";
+    System.out.println(output);
+    String str1 = "H311O";
+    String str2 = "wOr1d";
+    float f = 2.0f;
+    boolean t = true;
+    System.out.println(str1+" "+str2+" "+f+" "+t);
+    }
 }
 
