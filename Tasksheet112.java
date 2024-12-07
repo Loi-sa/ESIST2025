ef46fd0073f885d0af611d88746466a198636bea
Eloisa F. Perdiguerra <161401160+Loi-sa@users.noreply.github.com>
2024-12-07 10:56:13 +0000

    Done adding output

---
+++ Tasksheet112.java
@@ -0,0 +1,19 @@
+public class Tasksheet112 {
+   /* Create all of the primitives (except long and double) with different values. 
+Concatenate them into a string and  print it to the screen 
+so it will print: H3110 w0r1d 2.0 true
+*/
+    public static void main(String[] args) {
+            byte zero = 0;
+            String output ="W"+zero+"w";
+            System.out.println(output);
+     
+            String str1 = "H311O";
+            String str2 = "wOr1d";
+            float f = 2.0f;
+            boolean t = true;
+    
+            System.out.println(str1+" "+str2+" "+f+" "+t);  
+    }
+     
+}
