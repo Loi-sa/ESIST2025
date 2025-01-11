0f3de2c8318e2600c36748226f84cc2c16b116aa
Eloisa F. Perdiguerra <161401160+Loi-sa@users.noreply.github.com>
2025-01-11 06:05:38 +0000

    done adding output

--- Tasksheet121.java
+++ Tasksheet121.java
@@ -17,22 +17,22 @@ import static java.lang.Math.*;
     public class Tasksheet121 {
 
         public static int add(int a, int b) {
-            return addExact(a, b);
+            return Math.addExact(a, b);
         }
 
         public static int subtract(int a, int b) {
-            return subtractExact(a, b);
+            return Math.subtractExact(a, b);
         }
 
         public static int multiply(int a, int b) {
-            return multiplyExact(a, b);
+            return Math.multiplyExact(a, b);
         }
 
         public static int divide(int a, int b) {
             if (b == 0) {
                 throw new ArithmeticException("Division by zero");
             }
-            return floorDiv(a, b);
+            return Math.floorDiv(a, b);
         }
 
         public static void main(String[] args) {
